5212b2348f2682ccc2ceb7f96a721d5d464e2e5f
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-05-04 00:09:17 -0700

    Adds Font support

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          if (/\.(ttf|woff|woff2|eot|otf)$/.test(assetInfo.name)) {
-            return 'assets/fonts/[name]-[hash][extname]';
+        assetFileNames: ({ name }) => {
+          if (name && name.match(/\.(ttf|woff|woff2|eot|otf)$/)) {
+            return 'assets/fonts/[name][extname]';
           }
-          return 'assets/[name]-[hash][extname]';
+          return 'assets/[name][extname]';
         },
       },
     },
