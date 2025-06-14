87eb685c21ed71442d3b08f83f7be69dd50be2ac
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-06-14 19:30:20 -0400

    Adds padding tweaks to Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -30,13 +30,13 @@ const ReactProject = (props: ReactProject__Props) => {
     <>
       <div style={{fontFamily: heroFont}} className={`m-6 justify-center ${props.width < 750 ? "flex flex-col" : "flex"}`}>
 
-        <div className='md:w-[30%] md:max-h-[300px]'>
+        <div className=''>
           <img style={{ boxShadow: "5px 5px 5px rgb(35, 31, 31, .5)" }}
-            className="rounded-md"
+            className="rounded-md  md:max-h-[300px]"
             src="https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg" />
 
         </div>
-        <div className='ml-10'>
+        <div className={`ml-10 ${props.width < 750 ? "mt-8" : "flex flex-col justify-center mb-10" }`}>
           <h4 className='text-2xl'>"My Place"</h4>
           <p style={{fontFamily: tertiaryFont}}>Autodesk Maya and Arnold project.</p>
         </div>
