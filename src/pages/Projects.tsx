51d349ee264c9ad2a2015d11c252aca72b4f5016
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-06-14 17:46:42 -0400

    Adds basic projects template from old website

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,65 +4,90 @@ const Projects = () => {
   return (
     <div className="text-center" style={{ fontFamily: tertiaryFont }}>
       <h2>Projects</h2>
-
-
-      <a href="/index.html" className="text-link"><div><h1>Jay Crawford's Website</h1></div></a>
-
-
-
-      <div className="current-page"><h2>3D Projects</h2></div>
-
-
-
-
       <hr />
 
-      <div className="image-block">
+      <ReactProject />
+      <ReactProject />
+      <ReactProject />
 
-
-        <div><img className="blog-image-tab" src="https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg" /></div>
-
-        <div><h1 className="blog-post-title">"My Place"</h1>
-          <p className="blog-description">Autodesk Maya and Arnold project.</p></div>
-
-
-
-      </div>
-
-
-      <hr />
-
-      <div className="image-block">
-
-
-        <div><img className="blog-image-tab" src="https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/3d-door-project-icon.jpg" /></div>
-
-        <div><h1 className="blog-post-title">Door Project</h1>
-          <p className="blog-description">Based on a drawing, built in Blender to emulate the drawing.</p></div>
-
-
-
-      </div>
-
-      <hr />
-
-      <div className="image-block">
-
-
-        <div><img className="blog-image-tab" src="https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/3d-jay-icon.jpg" /></div>
-
-
-        <div><h1 className="blog-post-title">Jay VR Character</h1>
-          <p className="blog-description">Fully rigged, simple 3D character modeled after me. This helped me to better understand topology, weight-painting, and rigging.</p></div>
-
-
-
-      </div>
-
-      <hr />
     </div>
   )
 };
 
 
 export default Projects;
+
+
+const ReactProject = () => {
+  return (
+    <>
+      <style>
+        {`
+      .blog-image-tab {
+    border-radius: 5px;
+    box-shadow: 5px 5px 5px rgb(35, 31, 31, .5);
+    margin: 0rem 1.5rem 1.5rem 2rem;
+}
+
+@media only screen and (min-width: 601px) {
+    .image-block {
+        display: flex;
+    }
+
+    .image-block div .blog-post-title { 
+        padding: 1rem;
+    }
+
+
+    .blog-image-tab {
+        width: 14rem;
+        border-radius: 5px;
+    }
+
+    .blog-description {
+        padding: 0;
+        margin-left: 1rem;
+        margin-right: 2rem;
+    }
+
+    h1 {
+        padding: 1rem 0rem 0rem 1rem;
+    }
+
+    @media only screen and (max-width: 600px) {
+    .image-block {
+        display: block;
+    }
+    
+    .blog-image-tab {
+        display: block;
+        width: 80%;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    
+    .blog-post-title {
+        padding-left: 1rem;
+
+    }
+
+    
+    .blog-description {
+        padding: 0;
+        margin-left: 1rem;
+        margin-right: 2rem;
+    }
+}
+    
+}
+`}
+      </style>
+      <div className="p-2 block sm:flex">
+        <div ><img className="blog-image-tab" src="https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg" /></div>
+
+        <div><h1 className="blog-post-title">"My Place"</h1>
+          <p className="blog-description">Autodesk Maya and Arnold project.</p></div>
+      </div>
+      <hr /></>
+  )
+}
