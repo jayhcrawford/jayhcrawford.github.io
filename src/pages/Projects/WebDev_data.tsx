a5311252dcd2739dd2c18a0c771fdce4f48fd002
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2026-02-07 14:12:27 -0800

    update some webdev

--- src/pages/Projects/WebDev_data.tsx
+++ src/pages/Projects/WebDev_data.tsx
@@ -5,26 +5,26 @@ import { BlogItem } from "../../components/BlogList";
 export const web_dev_array: BlogItem[] = [
   {
     title: "Calculator",
-    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg",
-    about_txt: "Stub entry for the calculator web app.",
+    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/web-design-web-icons/Calc.webp",
+    about_txt: "CSS by me; Originally written with React class components while I did freeCodeCamp in 2023; rewritten into React 19 with an LLM.",
     url: "https://codepen.io/jayhcrawford/pen/rNRQJZa?editors=1010"
   },
   {
     title: "Chem 101 App",
-    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg",
-    about_txt: "My Chemistry 101 was taught by one of the most disorganized people I've ever met. This app was me taking and stand and refusing to allow shoddy and slow systems impede my grade. I got an A.",
+    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/web-design-web-icons/Chem101.webp",
+    about_txt: "Kind of like a study guide wiki. Essentially a markdown viewer/editor that allowed my classmates to create and propose revisions. Revisions were databased and reviewed using a seperately hosted API. Centralizing all the course content in an easy to access place helped me to get an A in this class.",
     url: "https://chem101-sg.vercel.app/index.html"
   },
   {
     title: "UrFriends",
     img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/blog-images/UR_Friends.webp",
-    about_txt: "Stub entry for the UrFriends web app.",
+    about_txt: "Will be a SaaS toolset for relationship building; inspired by the routines of world leaders. Started on MongoDB and Vercel; migrated to Amplify and AWS; settled on Firebase and Firestore. The hosted beta has auth and data privacy securely enforced and a Stripe webhook.",
     url: "/projects/urfriends",
   },
   {
     title: "Drum Machine",
-    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/3d-project-icons/orthographic-icon.jpg",
-    about_txt: "Stub entry for the drum machine web app.",
+    img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/web-design-web-icons/DM.webp",
+    about_txt: "CSS by me; Originally written with React class components while I did freeCodeCamp in 2023; rewritten into React 19 with an LLM.",
     url: "https://codepen.io/jayhcrawford/pen/poYJzyb"
   },
 ];
