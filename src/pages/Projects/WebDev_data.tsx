7d96b99f20c41fbcb10a6b6e06cbd380afa09873
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2026-02-07 22:25:20 -0800

    add first medium article

--- src/pages/Projects/WebDev_data.tsx
+++ src/pages/Projects/WebDev_data.tsx
@@ -7,7 +7,7 @@ export const web_dev_array: BlogItem[] = [
     title: "This Website!",
     img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/product-design-icons/jayhcrawford-domain.webp",
     about_txt: "This resposnive website uses Threejs, CSS, SVG, React, GitHub. I made the components.",
-    url: "this_website"
+    url: "https://medium.com/@jayhcrawford/taking-my-github-from-html-based-pages-to-a-vite-based-react-app-a202d7be2ad0"
 
   },
   {
