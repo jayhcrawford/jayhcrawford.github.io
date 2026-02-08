cd566abbb8bf7d58bd38ca3ee1e4d541c8a0b7c3
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2026-02-07 19:07:33 -0800

    update Webdev_data.tsx

--- src/pages/Projects/WebDev_data.tsx
+++ src/pages/Projects/WebDev_data.tsx
@@ -6,7 +6,7 @@ export const web_dev_array: BlogItem[] = [
   {
     title: "This Website!",
     img: "https://jayhcrawford-webimages.s3.us-east-2.amazonaws.com/web-icons/Web+Hosted/product-design-icons/jayhcrawford-domain.webp",
-    about_txt: "This resposnive website uses CSS, SVG, React, GitHub. I made the components.",
+    about_txt: "This resposnive website uses Threejs, CSS, SVG, React, GitHub. I made the components.",
     url: "this_website"
 
   },
