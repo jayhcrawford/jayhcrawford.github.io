bd472bac3db7790b457f6dde6a71b716e503430f
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2026-04-12 12:29:10 -0700

    add scrolltotop in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
+
+
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <App />
     </Router>
   </StrictMode>,
